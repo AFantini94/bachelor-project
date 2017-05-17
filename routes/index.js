746974c171aa8ab95fc838f223d2259eae027433
Alessandro Fantini <fanto@alessandros-macbook-pro.home>
2017-05-17 18:40:55 +0200

    Fixed /stream endpoint to POST.

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET stream page. */
-router.get('/stream', function(req, res, next) {
-  res.render('stream', { title: 'Stream' });
+router.post('/stream', function(req, res, next) {
+
+  res.render('stream', { title: 'Stream' + JSON.stringify(req.body)});
 });
 
 /* GET contentsource page. */
