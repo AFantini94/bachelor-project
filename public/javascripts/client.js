dccc3bf27d55e38135516bbf10a69f3ff59570ab
Alessandro Fantini <fanto@alessandros-mbp.mobile.usilu.net>
2017-05-17 19:05:24 +0200

    Script tag fixed, First AJAX call to newsriver created.

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -4,4 +4,16 @@ window.onload = function () {
 
     hsp.init({});
 
+    // ajax
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("risposta").innerHTML =
+                this.responseText;
+        }
+    };
+    xhttp.setRequestHeader("Authorization", "sBBqsGXiYgF0Db5OV5tAw6mQIvkRHsRXj1ydG2cLExfkfk3fuigx_ECw83pplNwx");
+    xhttp.open("GET", "https://api.newsriver.io/v2/search?query=text%3AJuventus&sortBy=_score&sortOrder=DESC&limit=15", true);
+    xhttp.send();
 };
