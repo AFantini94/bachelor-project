8037837f79b869629a99b282e364db55dabb777a
Alessandro Fantini <fanto@alessandros-macbook-pro.home>
2017-05-18 19:41:23 +0200

    Create article HTML container and CSS.

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,5 +1,61 @@
 // hootsuite
 
+function deleteAllChildNodes(root) {
+    while (root.firstChild) {
+        root.removeChild(root.firstChild);
+    }
+}
+
+function createArticleElement(article) {
+    var newArticle = document.createElement('div');
+    newArticle.className = 'article';
+
+    // find an image
+    var imageUrl = null;
+    article.elements.forEach(function (elem) {
+        if (elem.type === 'Image') {
+            imageUrl = elem.url;
+        }
+    });
+    if (imageUrl) {
+        var articleImage = document.createElement('img');
+        articleImage.className = 'article-image';
+        articleImage.src = imageUrl;
+        newArticle.appendChild(articleImage);
+    }
+
+    var articleTitle = document.createElement('a');
+    articleTitle.href = article.url;
+    articleTitle.className = 'article-title';
+    articleTitle.innerText = article.title;
+    newArticle.appendChild(articleTitle);
+
+    var articleText = document.createElement('div');
+    articleText.className = 'article-text';
+    articleText.innerText = article.text;
+
+    newArticle.appendChild(articleText);
+
+    var loadMoreBtn = document.createElement('div');
+    loadMoreBtn.className = 'load-more-btn';
+    loadMoreBtn.innerText = 'Show more...';
+    var showingMore = false;
+    loadMoreBtn.addEventListener('click', function() {
+        if (!showingMore) {
+            articleText.classList.add('article-text-full');
+            loadMoreBtn.innerText = 'Show less...';
+            showingMore = true;
+        } else {
+            articleText.classList.remove('article-text-full');
+            loadMoreBtn.innerText = 'Show more...';
+            showingMore = false;
+        }
+    });
+    newArticle.appendChild(loadMoreBtn);
+
+    return newArticle;
+}
+
 window.onload = function () {
 
     hsp.init({});
@@ -8,24 +64,17 @@ window.onload = function () {
 
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
+        if (this.readyState === 4 && this.status === 200) {
+            var articles = JSON.parse(this.response);
 
-            var json = JSON.parse(this.response);
-            document.getElementById("risposta").innerHTML = "";
-            json.forEach(function (article) {
-                // display title
-                var newArticle = document.createElement('div');
-                newArticle.innerHTML = article.title + '<br>';
-                newArticle.className = 'article';
-                // display image
-                var articleImage = document.createElement('img');
-                articleImage.onload = function () {
-                    articleImage.width = 300;
-                }
-                articleImage.src = article.elements[0].url;
-                newArticle.appendChild(articleImage);
-                document.body.appendChild(newArticle);
-            })
+            var articlesContainer = document.getElementById('articles-container');
+            deleteAllChildNodes(articlesContainer);
+
+            console.log(articlesContainer);
+
+            articles.forEach(function (article) {
+                articlesContainer.appendChild(createArticleElement(article));
+            });
         }
     };
     xhttp.open("GET", "https://api.newsriver.io/v2/search?query=text%3AJuventus&sortBy=_score&sortOrder=DESC&limit=15", true);
